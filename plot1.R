1f1abb5465ffb06a1e4529afb78ddfd682badcbd
fenom <fenom23@hotmail.com>
2015-12-11 18:33:49 +0000

    course project 1

---
+++ plot1.R
@@ -0,0 +1,6 @@
+dat <- read.csv2("household_power_consumption.txt", stringsAsFactors = FALSE)
+dat$Global_active_power <- (as.numeric(dat$Global_active_power))
+sub <- dat[dat$Date == "1/2/2007" | dat$Date == "2/2/2007",]
+png(file = "plot1.png", bg = "transparent")
+hist(sub$Global_active_power, col = "red", breaks = c(0.5*0:15), main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,11 @@
+dat <- read.csv2("household_power_consumption.txt", stringsAsFactors = FALSE)
+dat$Date_time <- strptime(paste(dat$Date,dat$Time),"%d/%m/%Y %H:%M:%S")
+dat$Global_active_power <- (as.numeric(dat$Global_active_power))
+sub <- dat[dat$Date == "1/2/2007" | dat$Date == "2/2/2007",]
+png(file = "plot3.png", bg = "transparent")
+plot(sub$Date_time, sub$Sub_metering_1, pch = NA, xlab = NA, ylab = "Energy sub metering")
+lines(sub$Date_time, sub$Sub_metering_1, col = "black")
+lines(sub$Date_time, sub$Sub_metering_2, col = "red")
+lines(sub$Date_time, sub$Sub_metering_3, col = "blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
